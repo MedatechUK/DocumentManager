33d4f700deddab156db524e6fad6d3847e7233d2
Simon Barnett <si@medatechuk.com>
2024-04-14 23:33:51 +0000

    Main

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ for f in [f for f in Path('s:/').glob('**/*') if f.is_file() and f.suffix =='.jp
     if ad:
         files.append(fi) # Add New file
 
-for f in [ f for f in files if not f.Wait() ]: # Pages > 2mins old
+for f in [ f for f in files if not f.Wait() ]: # Pages > 2mins old **
     p = f.NextPage() # Get First Page of file
     while p: # while has Page
         if p.barcode: # Page has Barcode?
             if f.hascanvas: c.save() # Save old PDF 
-            if p.preauth(): PREAUTH = True # Check page for PREAUTH
+            if p.preauth(): PREAUTH = True # Check page for PREAUTH **
             c = f.CreatePDF(p , preauth=PREAUTH) # Create new PDF
         else: # No barcode
            if f.hascanvas: c.showPage() # Add page break
@@ -29,8 +29,8 @@ for f in [ f for f in files if not f.Wait() ]: # Pages > 2mins old
         p = f.NextPage(p.pageid) # Get next page in file
     if f.hascanvas: c.save() # Save current PDF
     
-    for pdf in f.PDFfiles:
-        match pdf.hasqr:
-            case True   : pdf.dbSavePayCard() # Save to CoW/Payement
-            case _      : pdf.dbSave() # Save PDF to Document
+    for pdf in f.PDFfiles: # PDFs in this file
+        match pdf.hasqr: # Has QR Data? **
+            case True   : pdf.dbSavePayCard() # Save to CoW/Payement **
+            case _      : pdf.dbSave() # Save PDF to any Document
     f.delPages() # Clean Jpegs
